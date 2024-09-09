ce9cf05b64d2064efc6bd8ff4cf2de8d4e21697d
Terry Leggett II <terryleggettjr@gmail.com>
2024-09-09 12:03:25 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat() {
+    const copyOfCats = [...cats]
+    copyOfCats.push("Broom")
+    return copyOfCats
+}
+function prependCat() {
+    const copyCats = [...cats]
+    copyCats.unshift("Arnold")
+    return copyCats
+}
+function removeLastCat() {
+    const copyCats = [...cats]
+    copyCats.pop()
+    return copyCats
+}
+
+function removeFirstCat() {
+    const copyCats = [...cats]
+    copyCats.shift()
+    return copyCats
+}
